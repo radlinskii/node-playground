3e4b872a72f1c9ec404f3605b0857416c4da2278
radlinskii <radlinsk@student.agh.edu.pl>
2018-05-04 00:31:34 +0200

    :tada: child processes incoming!

---
+++ print-working-directory.js
@@ -0,0 +1,17 @@
+'use strict';
+const { spawn, } = require('child_process');
+
+const child = spawn('pwd');
+
+child.stdout.on('data', data => {
+  console.log(`child stdout: \n${data}`);
+});
+
+
+child.stderr.on('data', data => {
+  console.error(`child stderr: \n${data}`);
+});
+
+child.on('exit', (code, signal) => {
+  console.log(`child process exited with code ${code}, signal ${signal}`);
+});
