36e668d6eb80f342cb6e77b4e7208087fe361e91
radlinskii <radlinsk@student.agh.edu.pl>
2018-05-04 00:35:11 +0200

    :tada: simple non-blocking server!

---
+++ long-for-loop.js
@@ -0,0 +1,15 @@
+'use strict';
+const longComputation = () => {
+  let sum = 0;
+  for (let i = 0; i < 1e9; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+process.on('message', msg => { // eslint-disable-line 
+  const sum = longComputation();
+  process.send(sum);
+});
+
