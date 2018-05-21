a4f10328bd8c2e204157ae78f8f89e50ad453083
radlinskii <radlinsk@student.agh.edu.pl>
2018-05-21 12:07:07 +0200

    :zap: updated deling with promises

--- reading-file-using-async-await.js
+++ reading-file-using-async-await.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const readFile = () => {
   return new Promise((resolve, reject) => {
     fs.readFile('.eslintrc.json', (err, file) => {
-      return err ? reject(err) : resolve(file.toString());
+      return err ? reject(err) : resolve(file);
     });
   });
 };
 
 (async () => {
   try {
-    console.log(await readFile());
+    console.log(await readFile().toString());
   } catch (e) {
     console.log(e);
   }
--- reading-file-using-promise.js
+++ reading-file-using-promise.js
@@ -3,14 +3,17 @@ const fs = require('fs');
 const readFile = () => {
   return new Promise((resolve, reject) => {
     fs.readFile('.eslintrc.json', (err, file) => {
-      return err ? reject(err) : resolve(file.toString());
+      return err ? reject(err) : resolve(file);
     });
   });
 };
 
 readFile()
   .then(file => {
-    console.log(file);
+    return file.toString();
+  })
+  .then(string => {
+    console.log(string);
   })
   .catch(err => {
     console.log(err);
